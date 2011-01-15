b8f95a53c026bbdae4e8c577fc6af7833fd92479
Adriano Melo <github@adrianomelo.com>
2011-01-15 20:09:00 -0300

    DIAG mostrando os detalhes do paciente

--- src/iluminare/entrada/urls.py
+++ src/iluminare/entrada/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import * 
 
 urlpatterns = patterns('iluminare.entrada.views',
-    (r'^$', 'index')
+    (r'^$', 'index'),
+    (r'^search/(?P<nome>\w*)$', 'ajaxlistarpessoas'),
+    (r'^dialog/detalhe/(?P<paciente_id>[0-9]+)/(?P<tratamento_id>[0-9]+)$', 'dialog_detalhe')
 )
 
--- src/iluminare/entrada/views.py
+++ src/iluminare/entrada/views.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 # Create your views here.
 from django.shortcuts import render_to_response
-from iluminare.entrada.models import Paciente
+from django.http import HttpResponse
+from iluminare.entrada.models import *
 
 def ajaxlistarpessoas (request, nome):
     lista = []
@@ -18,6 +20,8 @@ def ajaxlistarpessoas (request, nome):
         salas = ["%s (%s)" % (t.tratamento.sala, t.tratamento.get_dia_display())  for t in tratamentos]
 
         dic = {
+			'tratamento_id': tratamentos and tratamentos[0].tratamento.id or 0,
+			'id':paciente.id,
             'nome':paciente.nome,
             'atendimentos': ", ".join(at_anteriores),
             'salas': ",".join(salas)
@@ -26,6 +30,23 @@ def ajaxlistarpessoas (request, nome):
         lista.append(dic)
     
     return render_to_response ('ajax-listar-pessoas.html', {'pacientes':lista})
+
+def dialog_detalhe(request, paciente_id, tratamento_id):
+    lista = TratamentoEmAndamento.objects.filter(tratamento__id=tratamento_id, paciente__id=paciente_id)
+    
+    if not lista:
+        return HttpResponse ("O tratamento do paciente não está cadastrado")
+
+    tratamento_em_andamento = lista[0]
+    sala = tratamento_em_andamento.tratamento.sala
+    nome = tratamento_em_andamento.paciente.nome
+	
+    dic = {
+        'nome': nome,
+        'sala': sala
+    }
+
+    return render_to_response ('ajax-dialog-detalhe-paciente.html', dic)
 
 def index(request):
     return render_to_response ('index.html')    
