6ea67dcd4747d5d2cce92b36d4d65a4911c1a73f
guilhermeamorim <guilherme.amorim@gmail.com>
2011-01-27 19:41:52 -0300

    Versao inicial

---
+++ src/carga_inicial/carrega_dados_pacientes.py
@@ -0,0 +1,133 @@
+
+#-*- coding: utf-8 -*-
+
+import MySQLdb
+
+# PARÂMETROS
+arquivo_sala1 = "Pacientes_QL_25_11_2010_sala1.csv"
+arquivo_sala2 = "Pacientes_QL_25_11_2010_sala2.csv"
+arquivo_sala3 = "Pacientes_QL_25_11_2010_sala3.csv"
+arquivo_sala4 = "Pacientes_QL_25_11_2010_sala4.csv"
+arquivo_sala5 = "Pacientes_QL_25_11_2010_sala5.csv"
+
+
+# retorna matriz com todas as linhas lidas no arquivo
+def le_arquivo(nome_arquivo):
+    file = open(nome_arquivo)
+    linhas = file.readlines()
+    matriz = []
+    for linha in linhas:
+        linha = linha.decode("cp1252") 
+        linha1 = linha.split(";",13)[1:13]
+        if linha1[0] == '':
+            break
+        matriz.append (linha.split(";",13)[1:13])
+        #print (linha.split(";",13)[0:13])
+    matriz = matriz[3:]
+    return matriz
+
+
+# retorna a lista somente com os nomes dos pacientes
+def lista_nomes(nome_arquivo):
+    matriz = le_arquivo(nome_arquivo)
+    lista = []
+    for linha in matriz:
+        nome_maiusculo = primeira_letra_maiuscula(linha[0])
+        lista.append(nome_maiusculo)
+        #print linha[1] 
+    return lista
+
+# Retorna o nome da pessoa com a primeira letra maiúscula
+# Excessão para palavras com 1 letra, 'da' e 'de'.
+# Evita que tenhamos João Da Silva ou João E Silva
+def letra_maiuscula(nome):
+    if len(nome) == 1:
+        return nome.lower()
+    elif nome.lower() == 'da':
+        return nome.lower()
+    elif nome.lower() == 'de':
+        return nome.lower()
+    else:
+        return nome.capitalize()
+
+# recebe como parêmetro um nome completo uppercase.
+# retorna o nome da pessoa somente com as primeiras letras maiúsculas
+# Ex: JOÃO DA SILVA -> João da Silva
+def primeira_letra_maiuscula(nome):
+    list = nome.split(' ')
+    # para cada item na lista, põe a primeira letra maiúscula
+    list2 = [letra_maiuscula(c) for c in list]
+    nome2 = ' '.join(list2)
+    return nome2
+
+
+# método que recebe como parâmetro a lista dos nomes dos pacientes e a sala
+# e insere os dados no banco de dados
+def insere_no_banco(lista, sala):
+    # Open database connection
+    db = MySQLdb.connect(host=".", port=3306, user="root", passwd="root", db="test")
+    #db = MySQLdb.connect('localhost','root','root','test',3306 )
+    
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+    
+    for nome in lista:
+        # foram usadas aspas duplas no comando insert devido a erro com nomes como 
+        # Joana D'arc
+        sql = "INSERT INTO paciente(nome, sala) \
+               VALUES (\"%s\", \"%s\")" % (nome, sala)
+        print sql
+        try:
+            # Execute the SQL command
+            cursor.execute(sql)
+            # Commit your changes in the database
+            db.commit()
+        except:
+            # Rollback in case there is any error
+            print "erro"
+            raw_input("Erro! Tecle ENTER para continuar...")
+            db.rollback()
+    db.close()
+    return
+
+
+# método que deve ser chamado para executar a carga de todos os pacientes
+# de todas as salas
+def executa():
+    nomes = lista_nomes(arquivo_sala1)
+    insere_no_banco(nomes, "1")
+    nomes = lista_nomes(arquivo_sala2)
+    insere_no_banco(nomes, "2")
+    nomes = lista_nomes(arquivo_sala3)
+    insere_no_banco(nomes, "3")
+    nomes = lista_nomes(arquivo_sala4)
+    insere_no_banco(nomes, "4")
+    nomes = lista_nomes(arquivo_sala5)
+    insere_no_banco(nomes, "5")
+    
+    
+    
+
+
+def main():
+    nomes = lista_nomes(arquivo_sala1)
+    
+    i = 1
+    for x in nomes:
+        print str(i) + '-' + x
+        i = i+1
+
+
+#main()
+executa()
+
+#nome = "JOAO DA SILVA"
+
+#print primeira_letra_maiuscula(nome)
+
+#print lista_nomes(matriz)
+
+
+
+#print matriz
+
