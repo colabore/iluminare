f5bc517c64e708bada769ae85bbfc3e26359e652
Adriano Melo <github@adrianomelo.com>
2011-03-27 16:42:21 -0300

    modulo clrf removido

--- src/iluminare/settings.py
+++ src/iluminare/settings.py
@@ -76,7 +76,6 @@ STATICFILES_DIRS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
