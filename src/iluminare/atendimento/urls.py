4a2565ce3e6530482f0b4b02070839af4346f642
Adriano Melo <github@adrianomelo.com>
2011-08-11 19:00:42 -0300

    correcao do urls.py

--- src/iluminare/atendimento/urls.py
+++ src/iluminare/atendimento/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('iluminare.atendimento.views',
     (r'^$', 'index'),
-    (r'^confirmacao$', 'confirmacao')
+    (r'^confirmacao$', 'confirmacao'),
 	(r'^consultar/(?P<paciente_id>[0-9]+)$','exibirAtendimentosPaciente')	
 )
 
