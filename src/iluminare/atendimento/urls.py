679d620f655c0bb95cf99a55b4a5c584ac403f65
root <root@luis-Aspire-4745.(none)>
2011-08-13 02:45:00 -0300

    funcionalidade para listar a fila de espera de cada tratamento

--- src/iluminare/atendimento/urls.py
+++ src/iluminare/atendimento/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('iluminare.atendimento.views',
 	(r'^$', 'index'),
-	(r'^consultar/(?P<paciente_id>[0-9]+)$','exibirAtendimentosPaciente')	
+	(r'^consultar/(?P<paciente_id>[0-9]+)$','exibir_atendimentos_paciente'),
+	(r'^listagem/(?P<pagina>[0-9]+)/$', 'exibir_listagem'),
+	(r'^listagem/$', 'exibir_listagem'),	
 )
 
--- src/iluminare/atendimento/views.py
+++ src/iluminare/atendimento/views.py
@@ -1,13 +1,98 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from iluminare.atendimento.models import *
+from iluminare.tratamento.models import *
+from iluminare.voluntario.models import *
+from django import forms
+import datetime
+from operator import itemgetter
+from django.core.paginator import Paginator
+
 
 def index(request):
 	return render_to_response('index.html')
 
 #codigo aqui
 
-def exibirAtendimentosPaciente(request, paciente_id):
+class ImprimirListagemForm(forms.Form):
+
+	def __init__(self, *args, **kwargs):
+        	super(ImprimirListagemForm, self).__init__(*args, **kwargs)
+        	self.fields['tratamento'].choices = [('', '----------')] + [(tratamento.id, tratamento.descricao_basica) for tratamento in Tratamento.objects.all()]
+
+	tratamento = forms.ChoiceField(choices=())
+	data = forms.DateField(initial = datetime.date.today)
+	
+
+def exibir_listagem(request, pagina = None):
+	
+	form_listagem = ImprimirListagemForm()
+	mensagem_erro = ''
+	retorno = [];
+	
+	if request.method == 'POST':
+		form_listagem = ImprimirListagemForm(request.POST)
+
+		if form_listagem.is_valid():
+			data_in = form_listagem.cleaned_data['data']
+			tratamento_in = form_listagem.cleaned_data['tratamento']
+			
+			tratamentos_marcados = InstanciaTratamento.objects.filter(tratamento__id  = tratamento_in, data = data_in)
+			
+			for tratamentos in tratamentos_marcados:
+				atendimentos_previstos = Atendimento.objects.filter(instancia_tratamento__id = tratamentos.id)
+				for atendimento in atendimentos_previstos:
+					try:					
+						prioridade = DetalhePrioridade.objects.get(paciente__id = atendimento.paciente.id)
+						voluntario = Voluntario.objects.get(paciente__id = atendimento.paciente.id)	
+						retorno.append({'nome': atendimento.paciente, 'hora': atendimento.hora_chegada, 'info': 						prioridade.get_prioridade_display() + '/' + voluntario.get_tipo_display(), 'prioridade': True})
+					except Voluntario.DoesNotExist:
+						retorno.append({'nome': atendimento.paciente, 'hora': atendimento.hora_chegada, 'info': 						prioridade.get_prioridade_display(), 'prioridade': True})
+							
+					except DetalhePrioridade.DoesNotExist:
+						try:					
+							voluntario = Voluntario.objects.get(paciente__id = atendimento.paciente.id)
+							if atendimento.prioridade:
+								retorno.append({'nome': atendimento.paciente, 'hora': atendimento.hora_chegada, 'info': 								voluntario.get_tipo_display() + '/' + atendimento.observacao_prioridade, 'prioridade': True})
+
+							else:
+								retorno.append({'nome': atendimento.paciente, 'hora': atendimento.hora_chegada, 'info': 								voluntario.get_tipo_display(), 'prioridade': False})
+						except Voluntario.DoesNotExist:	
+							if atendimento.prioridade:
+								retorno.append({'nome': atendimento.paciente, 'hora': atendimento.hora_chegada, 'info': 								atendimento.observacao_prioridade, 'prioridade': True})
+							else:					
+								retorno.append({'nome': atendimento.paciente, 'hora': atendimento.hora_chegada, 'info':'', 									'prioridade': False})
+			retorno_com_hora = [];
+			retorno_sem_hora = [];
+
+			for elemento in retorno:
+				if (elemento['hora'] == None):
+					retorno_sem_hora.append(elemento)
+				else:
+					retorno_com_hora.append(elemento)
+ 
+			retorno_com_hora = sorted(retorno_com_hora, key= itemgetter('hora'))
+			retorno = retorno_com_hora + retorno_sem_hora			
+			
+			
+			if not retorno:
+				mensagem_erro = 'não há registros'
+		else:
+			mensagem_erro = 'formulário inválido';
+
+	paginacao = Paginator(retorno,3) 
+	if pagina == None:
+		num_pagina = 1
+	else:	
+		num_pagina = int(pagina)
+	pagina_atual = paginacao.page(num_pagina)		
+
+	
+	return render_to_response('listagem-diaria.html', {'form_listagem':form_listagem, 
+							'mensagem': mensagem_erro,
+							'pagina_atual':pagina_atual})
+
+def exibir_atendimentos_paciente(request, paciente_id):
 	
 	lista_atendimentos = Atendimento.objects.filter(paciente__id = paciente_id)
 
