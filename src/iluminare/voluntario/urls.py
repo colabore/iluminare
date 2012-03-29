b6b3735568584efd41ec6bb0d6e0e33e3cc09227
Guilherme Amorim <guilherme.amorim@gmail.com>
2012-03-29 16:42:15 -0300

    Nota tela: consulta pontos dos voluntários

--- src/iluminare/voluntario/urls.py
+++ src/iluminare/voluntario/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('iluminare.voluntario.views',
     (r'^$', 'render'),
 	(r'^registra-ponto/$', 'registra_ponto'),
+	(r'^consulta-ponto/$', 'consulta_ponto'),
 )
 
